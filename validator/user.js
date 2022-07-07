1fc27c0221c79ea53b16fcc389a58545fa5ad5e2
Armin Sultana <arminsultana556@gmail.com>
2022-07-07 14:48:38 +0600

    Update commited

--- validator/user.js
+++ validator/user.js
@@ -61,24 +61,26 @@ exports.loginUser=[
 ];
 exports.updateValidator=[
     check('email')
-    .isLowercase()
-    .isEmail()
-    .withMessage('Invalid Email')
+    
     .custom(async(email)=>{
+       if(email){
         const output = validEmail(email);
         if(!output){
             throw 'Invalid email'
         }
+       }
     }),
 
-    // check('password')
-    // .trim()
-    // .custom(async(password)=>{
-    //     console.log(password);
-    //     if (password.length<8){
-    //         throw 'Please password must 8 character longer'
-    //     }
+    check('password')
+    .trim()
+    .custom(async(password)=>{
+       
+        if(password){
+            if (password.length<8){
+                throw 'Please password must 8 character longer'
+            }
+        }
 
-    // })
+    })
 
 ]
